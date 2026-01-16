25a82076338c548db0868f5272215bc40679affe
Vladislav Lyutin <dyson-sv25@yandex.ru>
2026-01-16 19:26:48 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.7" apply false
+	id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+group = "com.amit.mybankapp"
+version = "0.0.1"
+
+subprojects {
+
+	apply(plugin = "java")
+	apply(plugin = "io.spring.dependency-management")
+
+	repositories {
+		mavenCentral()
+	}
+
+	java {
+		toolchain {
+			languageVersion = JavaLanguageVersion.of(21)
+		}
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
+}
