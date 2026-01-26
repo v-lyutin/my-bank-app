cbe78fbb6cc9e772c277c1bf3bea88f25703861e
Vladislav Lyutin <dyson-sv25@yandex.ru>
2026-01-26 20:39:07 +0100

    refactor: change DTOs for TransferResource

--- accounts-service/src/main/java/com/amit/mybankapp/accounts/api/transfer/TransferController.java
+++ accounts-service/src/main/java/com/amit/mybankapp/accounts/api/transfer/TransferController.java
@@ -1,11 +1,11 @@
 package com.amit.mybankapp.accounts.api.transfer;
 
-import com.amit.mybankapp.accounts.api.transfer.dto.request.TransferRequest;
-import com.amit.mybankapp.accounts.api.transfer.dto.response.TransferResponse;
 import com.amit.mybankapp.accounts.api.transfer.mapper.TransferMapper;
 import com.amit.mybankapp.accounts.application.transfer.TransferUseCase;
 import com.amit.mybankapp.accounts.application.transfer.model.TransferResult;
 import com.amit.mybankapp.accounts.domain.wallet.vo.Money;
+import com.amit.mybankapp.commons.client.dto.transfer.CreateTransferRequest;
+import com.amit.mybankapp.commons.client.dto.transfer.CreateTransferResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -30,7 +30,7 @@ public class TransferController {
 
     // transfer-service only — internal
     @PostMapping
-    public ResponseEntity<TransferResponse> transfer(@Valid @RequestBody TransferRequest request) {
+    public ResponseEntity<CreateTransferResponse> createTransfer(@Valid @RequestBody CreateTransferRequest request) {
         TransferResult result = this.transferUseCase.transfer(request.recipientCustomerId(), Money.of(request.amount()));
         return ResponseEntity.ok(this.transferMapper.toTransferResponse(result));
     }
--- accounts-service/src/main/java/com/amit/mybankapp/accounts/api/transfer/mapper/TransferMapper.java
+++ accounts-service/src/main/java/com/amit/mybankapp/accounts/api/transfer/mapper/TransferMapper.java
@@ -1,17 +1,19 @@
 package com.amit.mybankapp.accounts.api.transfer.mapper;
 
-import com.amit.mybankapp.accounts.api.transfer.dto.response.TransferResponse;
 import com.amit.mybankapp.accounts.application.transfer.model.TransferResult;
+import com.amit.mybankapp.commons.client.dto.transfer.CreateTransferResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransferMapper {
 
-    public TransferResponse toTransferResponse(TransferResult result) {
-        return new TransferResponse(
+    public CreateTransferResponse toTransferResponse(TransferResult result) {
+        return new CreateTransferResponse(
+                null,
                 result.senderCustomerId(),
                 result.recipientCustomerId(),
-                result.amount()
+                result.amount(),
+                null
         );
     }
 
